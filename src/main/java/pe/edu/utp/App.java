f2f39787e1ce7f21f35c14b1c6131803b17568f2
manmanuel <azana.manuel@gmail.com>
2022-11-09 11:09:15 -0500

    primer commit

---
+++ src/main/java/pe/edu/utp/App.java
@@ -0,0 +1,16 @@
+package pe.edu.utp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        System.out.println("Mi proyecto");
+        System.out.println("Nuevamente");
+    }
+}
